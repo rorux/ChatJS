568522fc26a352d7654dd87515187ca5fbb8a7c1
RoRux <rorux84@gmail.com>
2022-07-23 21:21:00 +0300

    routing

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "dist")));
 
+app.get("/*", (req, res) => {
+  res.sendFile(__dirname + "/dist/index.html");
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
